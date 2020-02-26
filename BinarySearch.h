a0162315719cc591ef560bf062bc3b61ee6fdb74
Ratkhan Saginbazarov <s.ratkhan@gmail.com>
2020-02-26 00:10:17 -0500

    Added implementation of Binary search

---
+++ BinarySearch.h
@@ -0,0 +1,25 @@
+#pragma once
+//Implementation of Binary search using arrays
+
+#ifndef BINARYSEARCH_H
+#define BINARYSEARCH_H
+int binarySearch(int* array, int beginning, int end, int data);
+
+int search(int* array, int size, int data) {
+	return binarySearch(array, 0, size, data);
+}
+
+int binarySearch(int* array, int beginning, int end, int data)
+{
+	if (beginning == end) return -1;
+	if (data > array[(end + beginning) / 2]) {
+		binarySearch(array, ((end + beginning) / 2 + 1), end, data);
+	}
+	else if (data < array[(end + beginning) / 2]) {
+		binarySearch(array, beginning, ((end + beginning) / 2), data);
+	}
+	else return data;
+		
+}
+
+#endif
